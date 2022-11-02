ac3be16b2b757a6a72013ea7115802fa29551d3f
Bogdan Bogdan <bogdaiiikamekr@gmail.com>
2022-11-02 05:58:46 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Car_simulator;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector3 vector = new Vector3(1, 2, 3);
+            
+            Cars porshe = new Cars("porshe", vector, 0100);
+            porshe.KeyValid(0100);
+            porshe.Start();
+            porshe.Move(new Vector3(1, 3, 5));
+            porshe.Move(new Vector3(1, 3, 5));
+            Console.WriteLine(porshe.GetInfo());
+
+        }
+    }
+}
